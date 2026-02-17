b9df5d319d9a64b9bfa6e76f6e47afab122fb54f
Ahmed Matem <ahmedmatem@gmail.com>
2026-02-17 20:40:56 +0200

    Add Question dtos

---
+++ Quizle.Core/Dtos/QuestionIndexDto.cs
@@ -0,0 +1,12 @@
+﻿using Quizle.Core.Types;
+
+namespace Quizle.Core.Dtos
+{
+    public class QuestionIndexDto
+    {
+        public string? Search { get; set; }
+        public QuestionType? Type { get; set; }
+
+        public List<QuestionListItemDto> Items { get; set; } = new();
+    }
+}
---
+++ Quizle.Core/Dtos/QuestionListItemDto.cs
@@ -0,0 +1,13 @@
+﻿using Quizle.Core.Types;
+
+namespace Quizle.Core.Dtos
+{
+    public class QuestionListItemDto
+    {
+        public string Id { get; set; } = default!;
+        public string Text { get; set; } = default!;
+        public QuestionType Type { get; set; }
+        public int Points { get; set; }
+        public int OptionsCount { get; set; }
+    }
+}
